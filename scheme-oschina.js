b77c2409747a47ba4a71e0b3c4d787d94819a3f8
闲耘™ <hotoo.cn@gmail.com>
2015-07-05 09:25:09 +0900

    feat(oschina): Add oschina scheme

---
+++ scheme-oschina.js
@@ -0,0 +1,22 @@
+/* global module */
+module.exports = {
+  'home': '',
+  'issues': '/issues',
+  'issues/id': '/issues/{issue-id}',
+  'issues/new': '/issues/new',
+  'issues/new?title': '/issues/new?title={title}',
+  'wiki': '/wikis/home',
+  'network': '/network',
+  'tree': '/tree/{hash}',
+  'blob': '/blob/{hash}',
+  'commits': '/commits/master',
+  'commits-with-branch': '/commits/{branch-name}',
+  'milestones': '/milestones',
+  'tags': '/tags',
+  'releases': '/releases',
+  'releases/new': '/releases/new',
+  'releases/new-with-tag': '/releases/new?tag={tag}',
+  'pulls': '/pulls',
+  'pulls/new': '/compare',
+  'pulls/new-with-branch': '/compare/{branch-A}...{branch-B}',
+};
